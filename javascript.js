4ea869e5239a4bbc186b2928eb511371275e9840
David Cruz <dmd.cruz12@gmail.com>
2024-04-10 10:09:25 +0100

    Added logic and buttons to the game

--- javascript.js
+++ javascript.js
@@ -1,58 +1,102 @@
-//Computer Choice
-//Computer selects a random number between 0 and 0.9 and selects rock, paper or scissors depending on the randomized number
+let rounds = 0;
+let computerScoreValue = 0;
+let playerScoreValue = 0;
 
 
-function getComputerChoice(computerChoice) {
+const roundResult = document.querySelector(".result");
+const playerScore = document.querySelector(".playerScore");
+const computerScore = document.querySelector(".computerScore");
+
+const selectionBtn = document.querySelectorAll('button');
+selectionBtn.forEach((button) => {
+
+    button.addEventListener('click', () =>{   
+        let playerSelection = button.id;
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection,computerSelection);
+        rounds ++;
+        if (rounds == 5) {
+            determinesWinner(playerScoreValue,computerScoreValue);
+            selectionBtn.forEach(disablebtn => {
+                disablebtn.setAttribute("disabled", " ");
+            });
+        };
+    });
+ });
+
+ //Determines Full Game Winner!
+function determinesWinner(userScore,computerScore) {
+    let GameWinner;
     
-    //if (computerChoice < 0.3) {
-    //    return computerChoice = "rock"
-   // }
-    //else if (computerChoice <= 0.6) {
-      //  return computerChoice = "paper"
-    //}
-    //else {
-      //  return computerChoice = "scissors"
-    //}
-    
-    const choices = ["rock", "paper", "scissors"]
-    return  computerChoice =choices[Math.floor(Math.random()*choices.length)]
-}
+    if (userScore > computerScore) {
+        GameWinner = "Player Wins the game!"
+    };
+    if (userScore < computerScore) {
+        GameWinner = "Computer Wins the game!"
+    };
+    if (userScore == computerScore) {
+        GameWinner = "It's a tie!"
+    };
+
+    const divResult = document.createElement("div");
+    divResult.textContent = GameWinner;
+    const body = document.querySelector("body");
+    body.appendChild(divResult); 
+    //adicionar refresh btn and disable all the other buttons
+    const btnReset = document.createElement("button");
+    btnReset.textContent = 'Reset';
+    body.appendChild(btnReset);
+    btnReset.addEventListener('click', () =>{
+        location.reload();
+    });
+};
 
 
-
-//Determines the Winner
-
-function determineWinner(playerSelection,computerSelection) {
+//Determines the round winner!
+function playRound(playerSelection,computerSelection) {
     if (computerSelection === playerSelection.toLowerCase()) {
-        return "The game was a tie"
+        roundResult.textContent = roundResult.textContent + "Tie!";
     }
     if (playerSelection === "rock") {
         if (computerSelection === "paper") {
-            return "Computer wins!"
+            roundResult.textContent = "ROUND RESULT: " + "Computer wins!";
+            computerScoreValue += 1;
+            computerScore.textContent = "COMPUTER SCORE: " + computerScoreValue;
         }
         else {
+            roundResult.textContent = "ROUND RESULT: " + "The Player wins!";
+            playerScoreValue += 1;
+            playerScore.textContent = "PLAYER SCORE: " + playerScoreValue;
             return "The Player wins!"
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
-            return "Computer wins!"
+            roundResult.textContent = "ROUND RESULT: " + "Computer wins!";
+            computerScoreValue += 1;
+            computerScore.textContent = "COMPUTER SCORE: " + computerScoreValue;
         } else {
+            roundResult.textContent = "ROUND RESULT: " + "The Player wins!";
+            playerScoreValue += 1;
+            playerScore.textContent = "PLAYER SCORE: " + playerScoreValue;
             return "The Player wins!"
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
-            return "Computer wins!"
+            roundResult.textContent = "ROUND RESULT: " + "Computer wins!";
+            computerScoreValue += 1;
+            computerScore.textContent = "COMPUTER SCORE: " + computerScoreValue;
         } else {
-           return "The Player wins!"
+            roundResult.textContent = "ROUND RESULT: " + "The Player wins!";
+            playerScoreValue += 1;
+            playerScore.textContent = "PLAYER SCORE: " + playerScoreValue;
         }
     }
-}
+};
+//Computer Choice
+//Computer selects a random number between 0 and 0.9 and selects rock, paper or scissors depending on the randomized number
 
-//Play Game
-//alert (determineWinner(playerSelection, computerSelection))
-
-for (let index = 0; index < 5; index++) {
-    let playerSelection = prompt("Rock, Paper or Scissors? What do you choose?");
-    let computerSelection = getComputerChoice()
-    alert (determineWinner(playerSelection, computerSelection))
+function getComputerChoice(computerChoice) {
+    
+    const choices = ["rock", "paper", "scissors"]
+    return  computerChoice =choices[Math.floor(Math.random()*choices.length)]
 }
